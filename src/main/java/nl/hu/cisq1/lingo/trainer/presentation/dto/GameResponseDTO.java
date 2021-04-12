0dc790ccd32bdce6718904c43e0cd56f7b934414
JustMilan <milandol321@gmail.com>
2021-04-12 23:20:42 +0200

    Optimize gameResponseDTO

--- src/main/java/nl/hu/cisq1/lingo/trainer/presentation/dto/GameResponseDTO.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/presentation/dto/GameResponseDTO.java
@@ -5,13 +5,7 @@ import nl.hu.cisq1.lingo.trainer.domain.Game;
 import java.util.List;
 
 public class GameResponseDTO implements ResponseDTO {
-    private final List<Game> games;
 
     public GameResponseDTO(List<Game> games) {
-        this.games = games;
-    }
-
-    public List<Game> getGames() {
-        return games;
     }
 }
